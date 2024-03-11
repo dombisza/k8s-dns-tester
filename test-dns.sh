fbc4d242f81ed58d1613e3fe78aafe4cccfa6602
dombisza <dombisza@gmail.com>
2024-03-11 09:54:25 +0100

    add epoch

--- test-dns.sh
+++ test-dns.sh
@@ -6,6 +6,7 @@ counter=0
 host="ecs.eu-de.otc.t-systems.com"
 
 start_time=$(date +'%Y-%m-%dT%H:%M:%SZ')
+start_timestamp=$(date +"%s")
 while [ $counter -lt 1000 ]; do
     if nslookup $host >/dev/null 2>&1; then
       success=$(($success + 1))
@@ -16,10 +17,13 @@ while [ $counter -lt 1000 ]; do
     sleep 0.5
 done
 end_time=$(date +'%Y-%m-%dT%H:%M:%SZ')
+end_timestamp=$(date +"%s")
 if [ $counter -ne 0 ]; then
   success_rate=$(echo "scale=2; ($success / $counter) * 100" | bc)
 else
-  success_rate=100
+  echo "[ERROR] loop did not run?"
 fi
 
-echo "[$start_time --- $end_time] Success: $success Failed: $failed Rate: $success_rate"
+epoch_seconds=$((end_timestamp - start_timestamp))
+
+echo "[$start_time --- $end_time] Success: $success Failed: $failed Rate: ${success_rate}% Epoch: ${epoch_seconds}s"
