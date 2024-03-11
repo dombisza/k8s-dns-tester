83fdfc4b64efb2699701e30066ba09273f552040
dombisza <dombisza@gmail.com>
2024-03-11 09:36:33 +0100

    add timestamps

--- test-dns.sh
+++ test-dns.sh
@@ -3,9 +3,11 @@
 success=0
 failed=0
 counter=0
+host="ecs.eu-de.otc.t-systems.com"
 
+start_time=$(date +'%Y-%m-%dT%H:%M:%SZ')
 while [ $counter -lt 1000 ]; do
-    if nslookup ecs.eu-de.otc.t-systems.com >/dev/null 2>&1; then
+    if nslookup $host >/dev/null 2>&1; then
       success=$(($success + 1))
     else
       failed=$(($failed + 1))
@@ -13,11 +15,11 @@ while [ $counter -lt 1000 ]; do
       counter=$(($counter + 1))
     sleep 0.5
 done
-
+end_time=$(date +'%Y-%m-%dT%H:%M:%SZ')
 if [ $counter -ne 0 ]; then
   success_rate=$(echo "scale=2; ($success / $counter) * 100" | bc)
 else
   success_rate=100
 fi
 
-echo "Success: $success Failed: $failed Rate: $success_rate"
+echo "[$start_time --- $end_time] Success: $success Failed: $failed Rate: $success_rate"
